0422fc5127c9b791602640b0e552e8f1d242b96c
marcelovicentegc <marcelovicentegc@gmail.com>
2021-12-21 12:08:33 -0300

    chore: update cors config

--- my-app/middlewares/cors.ts
+++ my-app/middlewares/cors.ts
@@ -2,6 +2,10 @@ import Cors from "cors";
 
 const cors = Cors({
   methods: ["GET", "HEAD"],
+  origin: [
+    "http://localhost:3000",
+    "https://nextjs-run-time-integration-glowing-fiesta.vercel.app/",
+  ],
 });
 
 export { cors };
